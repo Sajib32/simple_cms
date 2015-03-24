fb73f27e2b5b50b02efd6a5c14339e0d339d5f0f
Sajib32 <Sajibnstu@gmail.com>
2015-03-24 23:03:52 +0600

    Selecting pages from the navigation

--- public/manage_content.php
+++ public/manage_content.php
@@ -1,22 +1,38 @@
 <?php require_once("../includes/db_connection.php") ?>
 <?php require_once("../includes/functions.php") ?>
 <?php include("../includes/layouts/header.php"); ?>
+
+<?php 
+	if (isset($_GET["subject"])) {
+		$selected_subject_id = $_GET["subject"];
+		$selected_page_id = null;
+	} elseif (isset($_GET["page"])) {
+		$selected_page_id = $_GET["page"];
+		$selected_subject_id = null;
+	} else {
+		$selected_subject_id = null;
+		$selected_page_id = null; 
+	}
+?>
 <div id="main">
 	<div id="navigation">
 		<ul class="subjects">
-			<?php 
-				$subject_set = find_all_subjects();
-			?>
+			<?php $subject_set = find_all_subjects(); ?>
 			<?php 
 				while($subject=mysqli_fetch_assoc($subject_set)) {
 			?>
-			<li><?php echo $subject["menu_name"] . " (" . $subject["id"] . ")"; ?>
-			<?php $page_set = find_pages_for_subject($subject["id"]);	?>
+			<li>
+				<a href="manage_content.php?subject=<?php echo urlencode($subject["id"]); ?>">
+				<?php echo $subject["menu_name"]; ?></a>
+				<?php $page_set = find_pages_for_subject($subject["id"]); ?>
 			<ul class="pages">
 				<?php 
 					while($page = mysqli_fetch_assoc($page_set)) {
 				?>
-				<li><?php echo $page["menu_name"]; ?></li>
+				<li>
+					<a href="manage_content.php?page=<?php echo urlencode($page["id"]); ?>">
+					<?php echo $page["menu_name"]; ?></a>
+				</li>
 				<?php 
 					}
 				?>
@@ -33,7 +49,8 @@
 	</div>
 	<div id="page">
 		<h2>Manage Content</h2>
-		
+		<?php echo $selected_subject_id; ?><br />
+		<?php echo $selected_page_id; ?>
 	</div>
 </div>
 <?php include("../includes/layouts/footer.php"); ?>	
