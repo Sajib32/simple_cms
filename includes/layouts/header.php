102f5d68eb02e28a4c415c6ec9c1372badcb04b5
Sajib32 <Sajibnstu@gmail.com>
2015-03-24 09:22:23 +0600

    Making Page Assets Reusable

---
+++ includes/layouts/header.php
@@ -0,0 +1,10 @@
+<DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Inspire Corp</title>
+		<link rel="stylesheet" type="text/css" media="all" href="stylesheets/public.css">
+	</head>
+	<body>
+		<div id="header">
+			<h1>Inspire Corp</h1>
+		</div>
--- public/admin.php
+++ public/admin.php
@@ -1,27 +1,17 @@
-<DOCTYPE html>
-<html lang="en">
-	<head>
-		<title>Untitled</title>
-		<link rel="stylesheet" type="text/css" media="all" href="stylesheets/public.css">
-	</head>
-	<body>
-		<div id="header">
-			<h1>Inspire Corp</h1>
-		</div>
-		<div id="main">
-			<div id="navigation">
-				&nbsp;
-			</div>
-			<div id="page">
-				<h2>Admin Menu</h2>
-				<p>Welcome to the Admin Area.</p>
-				<ul>
-					<li><a href="manage_content.php">Manage Website Content</a></li>
-					<li><a href="manage_admins.php">Manage Admin</a></li>
-					<li><a href="logout.php">Logout</a></li>
-				</ul>
-			</div>
-		</div>
-		<div id="footer">Copyright 20xx, Inspire Corp</div>
-	</body>
-</html>
+<?php require_once("../includes/functions.php") ?>
+<?php include("../includes/layouts/header.php") ?>
+<div id="main">
+	<div id="navigation">
+		&nbsp;
+	</div>
+	<div id="page">
+		<h2>Admin Menu</h2>
+		<p>Welcome to the Admin Area.</p>
+		<ul>
+			<li><a href="manage_content.php">Manage Website Content</a></li>
+			<li><a href="manage_admins.php">Manage Admin</a></li>
+			<li><a href="logout.php">Logout</a></li>
+		</ul>
+	</div>
+</div>
+<?php include("../includes/layouts/footer .php") ?>
