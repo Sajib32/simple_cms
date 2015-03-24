98e1efb538d556815fc8a2975fe25101953636a8
Sajib32 <Sajibnstu@gmail.com>
2015-03-25 00:50:53 +0600

    Highlighting the current page

--- public/manage_content.php
+++ public/manage_content.php
@@ -21,7 +21,13 @@
 			<?php 
 				while($subject=mysqli_fetch_assoc($subject_set)) {
 			?>
-			<li>
+			<?php 
+				echo "<li";
+				if ($subject["id"] == $selected_subject_id) {
+				 	echo " class=\"selected\"";
+				 } 
+				 echo ">";
+			 ?>
 				<a href="manage_content.php?subject=<?php echo urlencode($subject["id"]); ?>">
 				<?php echo $subject["menu_name"]; ?></a>
 				<?php $page_set = find_pages_for_subject($subject["id"]); ?>
@@ -29,7 +35,13 @@
 				<?php 
 					while($page = mysqli_fetch_assoc($page_set)) {
 				?>
-				<li>
+				<?php 
+					echo "<li";
+					if ($page["id"] == $selected_page_id) {
+						echo " class=\"selected\"";
+					}
+					echo ">";
+				?>
 					<a href="manage_content.php?page=<?php echo urlencode($page["id"]); ?>">
 					<?php echo $page["menu_name"]; ?></a>
 				</li>
