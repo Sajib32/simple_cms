7abe0f07cbf003e6706cc1f4dd438e3d24f8c3ba
Sajib32 <Sajibnstu@gmail.com>
2015-03-25 23:42:06 +0600

    Passing data to the session

--- public/create_subject.php
+++ public/create_subject.php
@@ -1,3 +1,4 @@
+<?php require_once("../includes/session.php"); ?>
 <?php require_once("../includes/db_connection.php"); ?>
 <?php require_once("../includes/functions.php"); ?>
 
@@ -21,12 +22,12 @@ if (isset($_POST['submit'])){
 
 	if ($result) {
 		// Success
-		$message = "Subject created."; 
+		$_SESSION["message"] = "Subject created."; 
 		redirect_to("manage_content.php");
 	} else {
 		// Failure
 		// $message = "Subject created failed";
-		$message = "Subject created failed.";
+		$_SESSION["message"] = "Subject created failed.";
 		redirect_to("new_subject.php");
 	}
 
--- public/new_subject.php
+++ public/new_subject.php
@@ -1,5 +1,6 @@
-<?php require_once("../includes/db_connection.php") ?>
-<?php require_once("../includes/functions.php") ?>
+<?php require_once("../includes/session.php"); ?>
+<?php require_once("../includes/db_connection.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
 <?php include("../includes/layouts/header.php"); ?>
 <?php find_selected_page(); ?>
 
@@ -8,6 +9,7 @@
 		<?php echo navigation($current_subject, $current_page); ?>
 	</div>
 	<div id="page">
+		<?php echo message(); ?>
 		<h2>Create Subject</h2>
 
 		<form action="create_subject.php" method="post">
